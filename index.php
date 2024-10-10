4f3fc448885eb390e35ca853572246978886045d
AleScla <scla.alessandro@gmail.com>
2024-10-10 22:35:26 +0200

    esercizio completato con bonus 1, da ordinare

--- index.php
+++ index.php
@@ -1,24 +1,48 @@
 <?php
     class Movie {
-        public $title;
+        private $title;
         public $actors;
         public $year;
-        public $genre;
+        public $genres;
         public $duration;
-    }
-    public function __construct(
-        string $title,
-        string|array $actors,
-        int $year,
-        string|array $genre,
-        int $duration
+
+        public function __construct(
+            string $title,
+            string|array $actors,
+            int $year,
+            string|array $genres,
+            string $duration
         ){
             $this->title = $title;
             $this->actors = $actors;
             $this->year = $year;
-            $this->genre = $genre;
+            $this->genres = $genres;
             $this->duration = $duration;
         }
+
+        public function setTitle($title){
+            if(strlen($title) > 1){
+                $this->title = $title;
+            }
+        }
+
+        public function getTitle(){
+            return $this->title;
+        }
+        
+    }
+    $actors = [
+        'Matthew McConaughey',
+        'Jessica Chastain',
+        'Anne Hathaway',
+        'Timothèe Chalamet'
+    ];
+    $genres = [
+        'Sci-fi',
+        'Avventura'
+    ];
+    $interstellar = new Movie('Interstellar', $actors, 2014, $genres, '2h 49 min');
+    var_dump($interstellar);
 ?>
 
 
@@ -31,6 +55,35 @@
         <title>PHP OOP 1</title>
     </head>
     <body>
-        
+        <ul>
+            <li>
+                <h1>
+                    Film
+                </h1>
+            </li>
+            <?php foreach ($interstellar as $singleItem){
+                if(!is_array($singleItem) && is_numeric($singleItem)){
+                    echo "<li> Anno:".$singleItem."</li>";
+                }
+                else if (is_array($singleItem) && $singleItem == $actors){
+                    foreach($singleItem as $key => $innerItem){
+                        echo "<li>Attore ".($key + 1).": ".$innerItem."</li>";
+                    }
+                }
+                else if(is_array($singleItem)&& $singleItem == $genres ){
+                    foreach($singleItem as $key => $innerItem){
+                        echo "<li>Genere ".($key + 1).": ".$innerItem."</li>";
+                    }
+                }
+                else{
+                    echo "<li> Durata:".$singleItem."</li>";
+                }
+            }
+            ?>
+            
+           
+           
+
+        </ul>
     </body>
 </html>
